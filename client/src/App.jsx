2501b3319179f87800e7b2e76c536375da80efbb
Omar Ahmed <omarahmed19991@outlook.com>
2024-08-13 14:02:09 +0300

    Add form to register which links with the backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function registerUser(event) {
+    event.preventDefault();
+    const response = await fetch("http://localhost:3000/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    })
+    const data = await response.json();
+    console.log(data);
+    
+  }
+
   return (
-    <div>App</div>
+    <div className='container'>
+      <h1>Register</h1>
+      <form onSubmit={registerUser}>
+        <input type="text" placeholder='Fullname' value={name} onChange={(e) => setName(e.target.value)}/>
+        <br />
+        <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <br />
+        <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <br />
+        <input type="submit" value="Register"/>
+      </form>
+    </div>
   )
 }
 
